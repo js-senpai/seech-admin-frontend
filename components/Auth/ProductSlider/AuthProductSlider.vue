058359aaba76021477b08d23f010a311eab18598
Andrew <fincheran@gmail.com>
2022-08-28 16:33:17 +0300

    Added small fixes to slider

--- components/Auth/ProductSlider/AuthProductSlider.vue
+++ components/Auth/ProductSlider/AuthProductSlider.vue
@@ -64,7 +64,7 @@ export default {
         [prop]: Math.random() * value,
         duration: 8,
         ease:'none',
-        filter: `blur(${gsap.utils.random(0,5)}px)`,
+        filter: `blur(${gsap.utils.random(0,3)}px)`,
         onComplete: this.randomMove,
         onCompleteParams: [target, prop, value],
       });
