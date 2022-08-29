f7980343ce8244be4d574fd80eaca1191cdff405
Andrew <fincheran@gmail.com>
2022-08-29 17:27:26 +0300

    test commit

--- components/Auth/ProductSlider/AuthProductSlider.vue
+++ components/Auth/ProductSlider/AuthProductSlider.vue
@@ -11,7 +11,14 @@
 export default {
   data: () => ({
     options: {
-      slidesPerView: 6,
+      breakpoints: {
+        320: {
+          sliderPerView: 5
+        },
+        768: {
+          slidesPerView: 6,
+        }
+      },
       loop: true,
       speed: 15000,
       autoplay: {
