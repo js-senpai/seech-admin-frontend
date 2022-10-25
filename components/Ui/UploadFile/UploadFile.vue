3e97267b45553b3748f8fcdd79b080e91a272631
Andrew <fincheran@gmail.com>
2022-10-25 11:30:08 +0300

    Added loader to UploadFile component

--- components/Ui/UploadFile/UploadFile.vue
+++ components/Ui/UploadFile/UploadFile.vue
@@ -1,10 +1,15 @@
 <template>
-  <label class="upload-file">
-    <input type="file" id="img" name="img" accept="image/png, image/jpeg" @change="upload()" class="d-none upload-file__input" ref="file">
-    <b-img-lazy src="~/assets/img/createRequestModal/download.svg" />
-    <span v-if="!getFile" class="upload-file__text">{{ $t('buttons.uploadPhoto') }}</span>
-    <b-img-lazy v-else class="upload-file__img" :src="getFile" />
-  </label>
+  <div class="upload-file">
+    <div class="d-flex justify-content-center">
+      <div v-show="isLoad" class="spinner-border" role="status" />
+    </div>
+    <label v-show="!isLoad" class="upload-file__label">
+      <input id="img" ref="file" type="file" name="img" accept="image/png, image/jpeg" class="d-none upload-file__input" @change="upload()">
+      <b-img-lazy src="~/assets/img/createRequestModal/download.svg" />
+      <span v-if="!getFile" class="upload-file__text">{{ $t('buttons.uploadPhoto') }}</span>
+      <b-img-lazy v-else class="upload-file__img" :src="getFile" />
+    </label>
+  </div>
 </template>
 <script>
 export default {
@@ -20,9 +25,9 @@ export default {
       default: ''
     },
   },
-  mounted() {
-    this.text = this.$i18n.t('buttons.uploadPhoto');
-  },
+  data: () => ({
+    isLoad: false
+  }),
   computed: {
     getFile: {
       get () {
@@ -33,8 +38,12 @@ export default {
       }
     },
   },
+  mounted() {
+    this.text = this.$i18n.t('buttons.uploadPhoto');
+  },
   methods: {
     async upload(){
+      this.isLoad = true;
       try {
         const newFile = this.$refs.file.files[0];
         const formData = new FormData();
@@ -43,6 +52,8 @@ export default {
         this.getFile = url;
       } catch (e) {
         console.error(e);
+      } finally {
+        this.isLoad = false;
       }
     }
   }
